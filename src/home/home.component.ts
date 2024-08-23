8dec14a8988f2c1a13993e63bd8845d45d38efaa
TCM-TI <development.teamcellmania@gmail.com>
2024-08-23 13:44:00 -0500

    creacion de dashboards

--- src/home/home.component.ts
+++ src/home/home.component.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
 import { FooterComponent } from '../app/footer/footer.component';
+import { ContenidoCentralComponent } from '../app/contenido-central/contenido-central.component';
+import { HeaderComponent} from '../app/header/header.component';
+import { SidebarComponent } from '../app/sidebar/sidebar.component';
+
 import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [FooterComponent],
+  imports: [FooterComponent, ContenidoCentralComponent, HeaderComponent, SidebarComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
